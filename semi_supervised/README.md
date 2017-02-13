daafc2e1b8a4ef443786d66c09845223ed07f776
Zheng Xu <xuzheng1111@gmail.com>
2017-02-13 17:58:28 -0600

    Make data script more flexible for preparing data.

--- semi_supervised/README.md
+++ semi_supervised/README.md
@@ -1,9 +1,45 @@
 # Prepare data
 
+Update: Change to new interfaces. New usage:
+
+## Build vocabulary
+
+Use the `build_vocab` switch to turn on building vocabulary functionality.
+
 ```bash
-python -m semi_supervised.data
+python -m semi_supervised.data --build_vocab 1 --smi_path /smile/nfs/projects/nih_drug/data/pm2/pm2.smi --vocab_path ~/expr/test/pretrain/pm2.vocab --out_path ~/expr/test/pretrain/pm2.tokens
 ```
 
+Example Output:
+```
+Creating temp file...
+Building vocabulary...
+Creating vocabulary /home/username/expr/test/pretrain/pm2.vocab from data /tmp/tmpcYVqV0
+  processing line 100000
+  processing line 200000
+  processing line 300000
+Translating vocabulary to tokens...
+Tokenizing data in /tmp/tmpcYVqV0
+  tokenizing line 100000
+  tokenizing line 200000
+  tokenizing line 300000
+```
+
+## Translate the SMI file using existing vocabulary
+
+Switch off `build_vocab` option, or simply hide it from the command line.
+
+```bash
+python -m semi_supervised.data --smi_path /smile/nfs/projects/nih_drug/data/logp/logp.smi --vocab_path ~/expr/test/pretrain/pm2.vocab --out_path ~/expr/test/pretrain/logp.tokens
+```
+
+Example Output:
+```
+Creating temp file...
+Reading vocabulary...
+Translating vocabulary to tokens...
+Tokenizing data in /tmp/tmpmP8R_P
+```
 
 # Train 
 
--- semi_supervised/data.py
+++ semi_supervised/data.py
@@ -3,21 +3,22 @@
 from __future__ import print_function
 
 import os
+import tempfile
 
 from tensorflow.models.rnn.translate.data_utils import (
     create_vocabulary, initialize_vocabulary, data_to_token_ids)
 import tensorflow as tf
 
 
-DEFAULT_DATA_DIR = os.path.expanduser("~/expr/seq2seq-fp/pretrain/")
-
-
 tf.app.flags.DEFINE_string(
-    "logp_path", "/smile/nfs/projects/nih_drug/data/logp/logp.smi", "logp data path.")
+    "smi_path", "/smile/nfs/projects/nih_drug/data/logp/logp.smi", "smi data path.")
 tf.app.flags.DEFINE_string(
-    "pm2_path", "/smile/nfs/projects/nih_drug/data/pm2/pm2.txt", "pm2 data path")
+    "vocab_path", "", "Vocabulary data_path.")
 tf.app.flags.DEFINE_string(
-    "data_dir", DEFAULT_DATA_DIR, "Pretrain data path, holding temporary data.")
+    "out_path", "", "Output token path.")
+tf.app.flags.DEFINE_bool(
+    "build_vocab", False, "Trigger the action: False for translating only. "
+    "If true, the script will build vocabulary and then translating.")
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -29,38 +30,28 @@ def mkdirp(dir_path):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
+def norm_path(ori_path):
+    """Normalize path."""
+    return os.path.expanduser(os.path.expandvars(ori_path))
 
-def logp_data_iter(logp_path=FLAGS.logp_path):
+def smi_data_iter(smi_path=FLAGS.smi_path):
     """Yield logp SMILE representation."""
-    with open(logp_path) as fobj:
+    with open(smi_path) as fobj:
         for line in fobj:
             if not len(line.strip()):
                 continue
             _smile = line.strip().split()[0]
             yield _smile
 
-
-def pm2_data_iter(pm2_path=FLAGS.pm2_path):
-    """Yield pm2 SMILE representation."""
-    with open(pm2_path) as fobj:
-        for line in fobj:
-            if not len(line.strip()):
-                continue
-            _smile = line.strip().split()[-1]
-            yield _smile
-
-
 def build_data_tmp(data_iter, data_path):
-    """Build temp data file inside the data_directory."""
+    """Build temp data file inside the data_directory. This is required for tensorflow function."""
     with open(data_path, "w+") as fobj:
         for _smile in data_iter:
             fobj.write("%s\n" % _smile)
 
-
 def smile_tokenizer(line):
     """Return each non-empty character as the token."""
     return list(line.strip().replace(" ", ""))
-
 
 def get_vocabulary(data_path, vocab_path):
     """Get the vocabulary for specific data temp file. If not, create one."""
@@ -70,32 +61,65 @@ def get_vocabulary(data_path, vocab_path):
     # Return the create vocabulary.
     return initialize_vocabulary(vocab_path)
 
+def check_output_path(path):
+    """Create folder if not exists."""
+    dir_path = os.path.dirname(path)
+    mkdirp(dir_path)
+    return dir_path
 
-def prepare_seq2seq_pretrain_data(data_dir=FLAGS.data_dir):
-    """Prepare the data iteration."""
-    mkdirp(data_dir)
-    # Build pm2 data set for training.
-    print("Building pm2 data set...")
-    pm2_data_tmp_path = os.path.join(data_dir, "pm2.tmp")
-    pm2_vocab_path = os.path.join(data_dir, "pm2.vocab")
-    pm2_tokens_path = os.path.join(data_dir, "pm2.tokens")
-    build_data_tmp(pm2_data_iter(), pm2_data_tmp_path)
-    get_vocabulary(pm2_data_tmp_path, pm2_vocab_path)
-    data_to_token_ids(pm2_data_tmp_path, pm2_tokens_path, pm2_vocab_path,
-                      tokenizer=smile_tokenizer, normalize_digits=False)
-    # Use logp data set for development and testing.
-    print("Building logp data set...")
-    logp_data_tmp_path = os.path.join(data_dir, "logp.tmp")
-    logp_tokens_path = os.path.join(data_dir, "logp.tokens")
-    build_data_tmp(logp_data_iter(), logp_data_tmp_path)
-    data_to_token_ids(logp_data_tmp_path, logp_tokens_path, pm2_vocab_path,
-                      tokenizer=smile_tokenizer, normalize_digits=False)
+def assert_path_exists(path):
+    """Make sure the path exists."""
+    assert os.path.exists(path), "Path does not exist: %s" % path
 
+def build_vocab(smi_path=FLAGS.smi_path,
+                vocab_path=FLAGS.vocab_path,
+                out_path=FLAGS.out_path):
+    """Build vocabulary for the given data."""
+    # create folder if needed.
+    assert_path_exists(smi_path)
+    check_output_path(vocab_path)
+    if out_path:
+        check_output_path(out_path)
+
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        tmp_path = tmp_file.name
+
+        data_iter = smi_data_iter(smi_path)
+        print("Creating temp file...")
+        build_data_tmp(data_iter, tmp_path)
+        print("Building vocabulary...")
+        get_vocabulary(tmp_path, vocab_path)
+        if out_path:
+            print("Translating vocabulary to tokens...")
+            data_to_token_ids(tmp_path, out_path, vocab_path,
+                              tokenizer=smile_tokenizer, normalize_digits=False)
+
+def translate_tokens(smi_path=FLAGS.smi_path,
+                     vocab_path=FLAGS.vocab_path,
+                     out_path=FLAGS.out_path):
+    """Output tokens from given vocab."""
+    assert_path_exists(smi_path)
+    assert_path_exists(vocab_path)
+    check_output_path(out_path)
+
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        tmp_path = tmp_file.name
+
+        data_iter = smi_data_iter(smi_path)
+        print("Creating temp file...")
+        build_data_tmp(data_iter, tmp_path)
+        print("Reading vocabulary...")
+        get_vocabulary(tmp_path, vocab_path)
+        print("Translating vocabulary to tokens...")
+        data_to_token_ids(tmp_path, out_path, vocab_path,
+                          tokenizer=smile_tokenizer, normalize_digits=False)
 
 def main(_):
-    """Main function for this file."""
-    prepare_seq2seq_pretrain_data()
-
+    """Entry function for this script."""
+    if FLAGS.build_vocab:
+        build_vocab()
+    else:
+        translate_tokens()
 
 if __name__ == "__main__":
     tf.app.run()
