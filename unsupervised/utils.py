c626023c523984836b214c5846fd8d78a778ec12
Your Name <xiaoyu>
2018-01-02 00:54:59 -0600

    fix "return else"

--- unsupervised/utils.py
+++ unsupervised/utils.py
@@ -110,8 +110,7 @@ def initialize_vocabulary(vocabulary_path):
         rev_vocab = [tf.compat.as_bytes(line.strip()) for line in rev_vocab]
         vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
         return vocab, rev_vocab
-    else:
-        raise ValueError("Vocabulary file %s not found.", vocabulary_path)
+    raise ValueError("Vocabulary file %s not found.", vocabulary_path)
 
 
 def sentence_to_token_ids(sentence, vocabulary,
